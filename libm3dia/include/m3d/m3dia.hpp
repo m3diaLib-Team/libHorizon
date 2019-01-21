69078bbe2f52a2c400f853edbf4529f21a2c7c77
Liam Minopulos <stunthacks@gmail.com>
2019-01-21 13:40:22 +0100

    Add master header

---
+++ libm3dia/include/m3d/m3dia.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file m3dia.hpp
+ * @brief The master header which includes all other files from the library
+ */
+#pragma once
+
+// switch libraries
+#include <switch.h>
+
+// m3Dia includes
