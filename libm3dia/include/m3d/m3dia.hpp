5c4cfee0ca9c0797a678f7b91473a964c1a67221
Liam Minopulos <stunthacks@gmail.com>
2019-02-05 12:27:57 +0100

    Add debug macros and nxlink

--- libm3dia/include/m3d/m3dia.hpp
+++ libm3dia/include/m3d/m3dia.hpp
@@ -4,7 +4,46 @@
  */
 #pragma once
 
-// switch libraries
-#include <switch.h>
-
 // m3Dia includes
+
+/**
+ * @def M3D_DEBUG
+ * @brief Enables NxLink and the M3D_DEBUGPOINT and M3D_BREAKPOINT macros
+ */
+
+#ifdef M3D_DEBUG
+    #include <unistd.h>
+    #define TRACE(fmt,...) printf("%s: " fmt "\n", __PRETTY_FUNCTION__, ## __VA_ARGS__)
+
+    static void initNxLink() {
+        if (R_FAILED(socketInitializeDefault()))
+            return;
+
+        m3d::priv::nxlinkSocket = nxlinkStdio();
+
+        if (m3d::priv::nxlinkSocket >= 0)
+            TRACE("printf output now goes to nxlink server");
+        else
+            socketExit();
+    }
+
+    static void deinitNxLink() {
+        if (m3d::priv::nxlinkSocket >= 0) {
+            close(m3d::priv::nxlinkSocket);
+            socketExit();
+            m3d::priv::nxlinkSocket = -1;
+        }
+    }
+
+    extern "C" void userAppInit() {
+        initNxLink();
+    }
+
+    extern "C" void userAppExit() {
+        deinitNxLink();
+    }
+#else
+    #define TRACE(fmt,...) ((void)0)
+    #define M3D_BREAKPOINT()
+    #define M3D_DEBUGPOINT()
+#endif
