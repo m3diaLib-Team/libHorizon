f853420b1db4d9ddaea54b397122bea991185bde
Liam Minopulos <stunthacks@gmail.com>
2019-02-21 12:26:03 +0100

    Add core include

---
+++ libhorizon/include/hzn/core/core.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file core.hpp
+ * @brief Includes all core files of libhorizon
+ */
+#ifndef CORE_H
+#define CORE_H
+
+#pragma once
+
+#include "applet.hpp"
+
+#endif /* end of include guard: CORE_H */
