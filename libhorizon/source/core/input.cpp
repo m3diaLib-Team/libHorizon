49f072fca824eaf2d8e86da1152fcc4eacc9fc18
Liam Minopulos <stunthacks@gmail.com>
2019-02-22 17:55:37 +0100

    :sparkles: Core: Check whether player is connected or not

--- libhorizon/source/core/input.cpp
+++ libhorizon/source/core/input.cpp
@@ -8,9 +8,19 @@ namespace horizon {
         u32 keysDown, keysHeld, keysUp;
 
         for(short int i = 0; i < (short int) Player::All; ++i) {
-            keysDown = hidKeysDown((HidControllerID) i);
-            keysHeld = hidKeysHeld((HidControllerID) i);
-            keysUp   = hidKeysUp((HidControllerID) i);
+            if (isPlayerConnected((Player) i)) {
+                keysDown = hidKeysDown((HidControllerID) i);
+                keysHeld = hidKeysHeld((HidControllerID) i);
+                keysUp   = hidKeysUp((HidControllerID) i);
+
+                m_inputData.playerData[i].controllerData.connected = true;
+            } else {
+                keysDown = 0;
+                keysHeld = 0;
+                keysUp   = 0;
+
+                m_inputData.playerData[i].controllerData.connected = false;
+            }
 
             m_inputData.pressedButtons  |= keysDown;
             m_inputData.downButtons     |= keysHeld;
