8ce252d7dbcce9a7e17c3e4a7ce97a032e56b8b5
Liam Minopulos <stunthacks@gmail.com>
2019-02-25 09:45:19 +0100

    :art: Core: Fix indentation

--- libhorizon/include/horizon/core/applet.hpp
+++ libhorizon/include/horizon/core/applet.hpp
@@ -13,68 +13,68 @@
  * @brief The general horizon-namespace
  */
 namespace horizon {
-   /**
-    * @brief The applet class (used for running the app)
-    */
-   class Applet {
-   public:
-       /**
-        * @brief Creates the applet
-        * @param enableNxlinkStdio Whether you want to enable nxlinkStdio or not
-        */
-       Applet(bool enableNxlinkStdio = true);
+    /**
+     * @brief The applet class (used for running the app)
+     */
+    class Applet {
+    public:
 
-       /**
-        * @brief Destructs the applet
-        */
-       ~Applet();
+        /**
+         * @brief Creates the applet
+         * @param enableNxlinkStdio Whether you want to enable nxlinkStdio or not
+         */
+        Applet(bool enableNxlinkStdio = true);
 
-       /**
-        * @brief Returns if the applet is still running
-        */
-       bool isRunning();
+        /**
+         * @brief Destructs the applet
+         */
+        ~Applet();
 
-       /**
-        * @brief Exits the applet
-        */
-       void exit();
+        /**
+         * @brief Returns if the applet is still running
+         */
+        bool isRunning();
 
-       /**
-        * @brief Sets whether or not to allow the use of the home button
-        * @param allow `true` if the user is allowed to use the home button, `false` otherwise
-        */
-       static inline void allowHomeButton(bool allow) {
-           if (allow) {
-               appletEndBlockingHomeButton();
-           } else {
-               appletBeginBlockingHomeButton(0);
-           }
-       }
+        /**
+         * @brief Exits the applet
+         */
+        void exit();
 
-       /**
-        * @brief Enables/disables video recording
-        * @param enable Whether or not to enable video recording
-        */
-       static inline void enableVideoRecording(bool enable) {
-           if (enable) {
-               appletSetGamePlayRecordingState(1);
-           } else {
-               appletSetGamePlayRecordingState(0);
-           }
-       }
+        /**
+         * @brief Sets whether or not to allow the use of the home button
+         * @param allow `true` if the user is allowed to use the home button, `false` otherwise
+         */
+        static inline void allowHomeButton(bool allow) {
+            if (allow) {
+                appletEndBlockingHomeButton();
+            } else {
+                appletBeginBlockingHomeButton(0);
+            }
+        }
 
-       /**
-        * @brief Returns the current frame
-        * @return The currently drawn frame (ranging from 0 to 59)
-        */
-       int getCurrentFrame();
+        /**
+         * @brief Enables/disables video recording
+         * @param enable Whether or not to enable video recording
+         */
+        static inline void enableVideoRecording(bool enable) {
+            if (enable) {
+                appletSetGamePlayRecordingState(1);
+            } else {
+                appletSetGamePlayRecordingState(0);
+            }
+        }
+
+        /**
+         * @brief Returns the current frame
+         * @return The currently drawn frame (ranging from 0 to 59)
+         */
+        int getCurrentFrame();
 
     private:
-       /* data */
-       bool m_running;
-       int m_currentFrame;
-
-   };
+        /* data */
+        bool m_running;
+        int m_currentFrame;
+    };
 } /* horizon */
 
 #endif /* end of include guard: APPLET_H */
