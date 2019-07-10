a0b72b054b3db47d021ebede903c22a2d876a9f2
Liam Minopulos <stunthacks@gmail.com>
2019-07-10 14:23:45 +0200

    :sparkles: Core: Add support for left Joy-Con

--- libhorizon/source/core/input.cpp
+++ libhorizon/source/core/input.cpp
@@ -1,5 +1,11 @@
 #include "horizon/core/input.hpp"
 
+#define DPAD_TO_LETTER(mask, direction, letter) \
+    if (mask & direction) {          \
+        mask &= ~direction;          \
+        mask &= letter;              \
+    }
+
 namespace horizon {
     Input::InputData Input::m_inputData;
 
@@ -16,6 +22,29 @@ namespace horizon {
                 keysDown = hidKeysDown((HidControllerID) i);
                 keysHeld = hidKeysHeld((HidControllerID) i);
                 keysUp   = hidKeysUp((HidControllerID) i);
+
+                // since there are only d-pad buttons on the left Joy-Con, we need to convert those to A/B/X/Y-presses
+                if (hidGetControllerType((HidControllerID) i) == TYPE_JOYCON_LEFT) {
+                    // Y
+                    DPAD_TO_LETTER(keysDown, Button::DPadLeft, Button::Y)
+                    DPAD_TO_LETTER(keysHeld, Button::DPadLeft, Button::Y)
+                    DPAD_TO_LETTER(keysUp, Button::DPadLeft, Button::Y)
+
+                    // A
+                    DPAD_TO_LETTER(keysDown, Button::DPadRight, Button::A)
+                    DPAD_TO_LETTER(keysHeld, Button::DPadRight, Button::A)
+                    DPAD_TO_LETTER(keysUp, Button::DPadRight, Button::A)
+
+                    // X
+                    DPAD_TO_LETTER(keysDown, Button::DPadUp, Button::X)
+                    DPAD_TO_LETTER(keysHeld, Button::DPadUp, Button::X)
+                    DPAD_TO_LETTER(keysUp, Button::DPadUp, Button::X)
+
+                    // B
+                    DPAD_TO_LETTER(keysDown, Button::DPadDown, Button::B)
+                    DPAD_TO_LETTER(keysHeld, Button::DPadDown, Button::B)
+                    DPAD_TO_LETTER(keysUp, Button::DPadDown, Button::B)
+                }
 
                 m_inputData.playerData[i].controllerData.connected = true;
             } else {
