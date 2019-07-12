b43cfda8ce4eaee3d52fb26150812539765f727e
Liam Minopulos <stunthacks@gmail.com>
2019-07-12 12:20:12 +0200

    :sparkles: Graphics: Implement RenderTarget

---
+++ libhorizon/include/horizon/graphics/renderTarget.hpp
@@ -0,0 +1,35 @@
+/**
+ * @file renderTarget.hpp
+ * @brief Defines the rendertarget class
+ */
+#ifndef RENDERTARGET_HPP
+#define RENDERTARGET_HPP
+
+#pragma once
+
+namespace horizon {
+    /**
+     * @brief Can be used to render something without directly displaying it on screen
+     */
+    class RenderTarget {
+    public:
+        /**
+         * @brief Creates the rendertarget
+         */
+        RenderTarget();
+
+        /**
+         * @brief Binds the rendertarget
+         *
+         * This causes all rendering to be redirected to this rendertarget
+         */
+        void bind();
+
+    private:
+        /* data */
+        unsigned int m_framebuffer;
+    };
+} /* horizon */
+
+
+#endif /* end of include guard: RENDERTARGET_HPP */
---
+++ libhorizon/source/graphics/renderTarget.cpp
@@ -0,0 +1,12 @@
+#include "horizon/graphics/renderTarget.hpp"
+#include "horizon/private/opengl.hpp"
+
+namespace horizon {
+    RenderTarget::RenderTarget() {
+        glGenFramebuffers(1, &m_framebuffer);
+    }
+
+    void RenderTarget::bind() {
+        glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer);
+    }
+} /* horizon */
