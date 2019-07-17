96a4f4b2d898e6e6ead3c8d90ebef1422c62c032
Liam Minopulos <stunthacks@gmail.com>
2019-07-17 14:37:40 +0200

    :sparkles: Graphics: Implement ShaderProgram

---
+++ libhorizon/include/horizon/graphics/shaderProgram.hpp
@@ -0,0 +1,76 @@
+/**
+ * @file shaderProgram.hpp
+ * @brief Defines the shader-program class
+ */
+#ifndef SHADERPROGRAM_HPP
+#define SHADERPROGRAM_HPP
+
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+
+namespace horizon {
+    class Shader;
+
+    /**
+     * @brief Combines multiple different shaders in order to use them in union
+     *
+     * In order to load and switch between shader-programs easier, please refer to horizon::ShaderManager.
+     */
+    class ShaderProgram {
+    public:
+        /*
+         * @brief Constructs the shader program
+         * @param name The name of the program, for the shader manager. See horizon::ShaderManager for more information
+         *
+         * Leave `name` blank if you don't want to add the program to the shader manager.
+         */
+        explicit ShaderProgram(const std::string& name = "");
+
+        /**
+         * @brief Destructs the shader program
+         */
+        ~ShaderProgram();
+
+        /**
+         * @brief Returns the name of the program
+         * @return The name
+         *
+         * For more information, please refer to horizon::ShaderManager.
+         */
+        const std::string getName();
+
+        /**
+         * @brief Attaches the given shader to the program
+         * @param shader The shader to attach
+         */
+        void attach(const horizon::Shader& shader);
+
+        /**
+         * @brief Links the shader program
+         * @return       Whether the linking was successful
+         * @retval true  The program was successfully linked
+         * @retval false The linking of the program resulted in an error
+         */
+        bool link();
+
+        /**
+         * @brief Resets the program ("un-attaches" all shaders)
+         */
+        void reset();
+
+        /**
+         * @brief Uses the shader program
+         */
+        void use();
+
+    private:
+        /* data */
+        std::string m_name;
+        GLuint m_program;
+    };
+} /* horizon */
+
+
+#endif /* end of include guard: SHADERPROGRAM_HPP */
---
+++ libhorizon/source/graphics/shaderProgram.cpp
@@ -0,0 +1,52 @@
+#include "horizon/graphics/shaderProgram.hpp"
+#include "horizon/graphics/shader.hpp"
+
+#include <stdio.h>
+
+namespace horizon {
+    ShaderProgram::ShaderProgram(const std::string& name) :
+        m_name(name) {
+        m_program = glCreateProgram();
+
+        if (name != "") {
+            // TODO: Add program to shader manager
+        }
+    }
+
+    ShaderProgram::~ShaderProgram() {
+        glDeleteProgram(m_program);
+    }
+
+    const std::string ShaderProgram::getName() {
+        return m_name;
+    }
+
+    void ShaderProgram::attach(const horizon::Shader& shader) {
+        glAttachShader(m_program, shader.getShader());
+    }
+
+    bool ShaderProgram::link() {
+        glLinkProgram(m_program);
+
+        GLint success;
+        glGetProgramiv(m_program, GL_LINK_STATUS, &success);
+
+        if (!success) {
+            char buf[512];
+            glGetProgramInfoLog(m_program, sizeof(buf), nullptr, buf);
+            printf("ShaderProgram: Link error: %s", buf);
+            return false;
+        }
+
+        return true;
+    }
+
+    void ShaderProgram::reset() {
+        glDeleteProgram(m_program);
+        m_program = glCreateProgram();
+    }
+
+    void ShaderProgram::use() {
+        glUseProgram(m_program);
+    }
+} /* horizon */
